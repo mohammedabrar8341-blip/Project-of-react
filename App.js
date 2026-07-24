1524072b58635f7a413254c2c12c2bbe32a7958a
mohammedabrar8341-blip <mohammedabrar8341@gmail.com>
2026-07-24 10:05:58 +0530

    Context Api, simple provider using .Provider (guest,username)

--- App.js
+++ App.js
@@ -5,15 +5,38 @@ import RestaurantCard from "./src/component/RestaurantCard";
 import Footer from "./src/component/Footer";
 import Body from "./src/component/Body";
 import { Outlet } from "react-router";
+import UseContext from "./utlis/UseContext";
+import { useState, useEffect } from "react";
 
-export default function App() {
+const App = () => {
+  const [username, setUsername] = useState();
+  const [email, setemail] = useState();
+  //authentication
+  useEffect(() => {
+    const randomTrue = Math.random() < 0.3;
+    if (!randomTrue) {
+      setUsername("guest");
+      setemail("guestemail.com");
+      return;
+    }
+    //data from backend if authentication call sucess
+    const loggedInuser = {
+      name: "M. Abrar",
+      email: "mohammedabrar@gmail.com",
+    };
+    setUsername(loggedInuser.name);
+    setemail(loggedInuser.email);
+  }, []);
   return (
     <div>
-      <Header />
-      {/* <Img /> */}
-      <Outlet />
-      {/* <Body /> */}
-      <Footer />
+      <UseContext.Provider value={{ name: username, email: email }}>
+        <Header />
+        {/* <Img /> */}
+        <Outlet />
+        {/* <Body /> */}
+        <Footer />
+      </UseContext.Provider>
     </div>
   );
-}
+};
+export default App;
